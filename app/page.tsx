10145b0a357435aaa7272470ca64531c6bff756c
Jonathan Lizama <jonathan.b.lizama@gmail.com>
2023-09-17 02:05:12 -0500

    Added GLSL Shader

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import React, { useRef, useState, useEffect, useMemo } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
+import { useTexture, shaderMaterial } from "@react-three/drei";
+import axios from "axios";
+import './styles.css'
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface RotatingCubeProps {
+  vertex: string;
+  fragment: string;
 }
+
+function RotatingCube({ vertex, fragment }: RotatingCubeProps): JSX.Element {
+  const cubeRef = useRef<THREE.Mesh>(null);
+  const materialRef = useRef<THREE.ShaderMaterial>(null);
+  const [hovered, setHover] = useState(false);
+  const [clicked, click] = useState(false);
+  
+  // Rotate the cube in the render loop
+  useFrame(() => {
+    if (cubeRef.current) {
+      cubeRef.current.rotation.x += 0.00;
+      cubeRef.current.rotation.y += 0.00;
+    }
+  });
+
+    // Load the noise texture and update the shader uniform
+  const noiseTexture = useTexture("noise2.png");
+  useFrame((state) => {
+    let time = state.clock.getElapsedTime();
+    if (materialRef.current) {
+      materialRef.current.uniforms.iTime.value = time + 100;
+    }
+  });
+
+    // Define the shader uniforms with memoization to optimize performance
+    const uniforms = useMemo(
+      () => ({
+        iTime: {
+          type: "f",
+          value: 1.0,
+        },
+        iResolution: {
+          type: "v2",
+          value: new THREE.Vector2(2, 1.5),
+        },
+        iChannel0: {
+          type: "t",
+          value: noiseTexture,
+        },
+      }),
+      []
+    );
+
+
+  return (
+    <mesh
+      ref={cubeRef}
+      position={[0, 0, 0]} 
+      scale={1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <planeGeometry args={[75, 75]} />
+      <shaderMaterial
+        ref={materialRef}
+        uniforms={uniforms}
+        fragmentShader={fragment}
+        vertexShader={vertex}
+        side={THREE.DoubleSide}
+      />
+    </mesh>
+  );
+}
+
+function ThreeScene(): JSX.Element {
+   
+  // State variables to store the vertex and fragment shaders as strings
+   const [vertex, setVertexShader] = useState("");
+   const [fragment, setFragmentShader] = useState("");
+
+   // Fetch the shaders once the component mounts
+  useEffect(() => {
+    // fetch the vertex and fragment shaders from public folder 
+    axios.get("/vertexShader.glsl").then((res) => setVertexShader(res.data));
+    axios.get("/fragmentShader.glsl").then((res) => setFragmentShader(res.data));
+  }, []);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+    <Canvas>
+      <ambientLight />
+      <RotatingCube vertex={vertex} fragment={fragment} />
+    </Canvas>
+    </div>
+  );
+}
+
+export default ThreeScene;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {transpilePackages:['three'], webpack: (config) => {config.module.rules.push({
+    test: /\.(frag|vert|glsl)$/,
+    type: 'asset/source',
+    use: ['glslify-loader'],
+    })
+    return config
+    }
+}
 module.exports = nextConfig
